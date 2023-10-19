c759e83aaff7fd3af005578bf6b8d21e61ea50c7
Sarunas Staskevicius <sarunas.staskevicius@gmail.com>
2023-10-19 17:22:12 +0300

    add code improve

--- ex1.py
+++ ex1.py
@@ -4,17 +4,21 @@ def sum_of_three_numbers(a: int, b: int, c: int) -> int:
     return a + b + c
 print(sum_of_three_numbers(5, 10, 20))
 
+
 def about_me(name: str, surname: str, age: int) -> str:
     return name + " "  + surname + " " + str(age)
 print(about_me("Sarunas", "Staskevicius", 33))
+
 
 def letters_a_count(text: str) -> int:
     return(len([word for word in text.split(" ") if "a" in word]))
 print(letters_a_count("Laba diena kaip jums sekasi"))
 
+
 def separate(text: str) -> str:
     return text.replace(" ", "|")
 print(separate("Mano vardas yra Sarunas"))
+
 
 def round_my_number(number: float) -> float:
     return round(number, 3)
--- ex2.py
+++ ex2.py
@@ -1,4 +1,9 @@
 # Create a function that adds a string ending to each member in a list.
 
-def add_string_to_list(text: str, number_list: list) -> list:
-    
+def add_string_to_list(random_list: list, text: str) -> list:
+   return [item + text for item in random_list]
+
+random_list = ["green", "white", "red", "blue", "grey"]
+text = "color"
+print(add_string_to_list(random_list, text))
+
